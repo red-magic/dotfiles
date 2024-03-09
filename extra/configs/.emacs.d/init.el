aa4351c0eb8af67c820627391d7f15b257ff46eb
Egor Gushcha <egorgushcha@proton.me>
2024-03-10 01:04:09 +0600

    Update stuff

--- extra/configs/.emacs.d/init.el
+++ extra/configs/.emacs.d/init.el
@@ -11,7 +11,17 @@
  '(size-indication-mode t)
  '(use-short-answers t))
 
-;;(custom-set-faces
-;; '(italic ((t (:foreground "blue")))))
+(custom-set-faces
+ '(italic ((t (:foreground "blue"))))
+ '(shr-h3 ((t (:foreground "blue")))))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(advice-add 'eww--dwim-expand-url
+	    :around
+	    (lambda (orig &rest args)
+	      "Use https"
+	      (let ((orig-url (apply orig args)))
+		(if (string-match "http://" orig-url)
+		    (replace-match "https://" nil nil orig-url)
+		  orig-url))))
