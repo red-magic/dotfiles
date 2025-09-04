81d95e8e9ab9c8698804348199e01ce00fa48da2
Egor Gushcha <egorgushcha@proton.me>
2025-09-05 04:26:37 +0600

    init.el: update

--- extra/configs/.emacs.d/init.el
+++ extra/configs/.emacs.d/init.el
@@ -6,10 +6,17 @@
  '(make-backup-files nil)
  '(menu-bar-mode nil)
  '(require-final-newline t)
- '(sh-indentaion 8)
  '(show-paren-mode nil)
  '(size-indication-mode t)
  '(use-short-answers t))
+
+(add-hook 'sh-mode-hook
+	  (lambda ()
+	    (setq sh-basic-offset 4)
+	    (setq sh-indentaion 4)
+	    (setq indent-tabs-mode nil)))
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (custom-set-faces
  '(isearch ((t (:background "blue" :foreground "black"))))
@@ -17,8 +24,6 @@
  '(lazy-highlight ((t (:background "blue" :foreground "black"))))
  '(region ((t (:background "blue" :foreground "black"))))
  '(shr-h3 ((t (:foreground "blue")))))
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (advice-add 'eww--dwim-expand-url
 	    :around
