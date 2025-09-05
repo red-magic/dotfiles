87bf3354e3397a5b95f428fd378996c42357c806
Egor Gushcha <egorgushcha@proton.me>
2025-09-05 08:43:40 +0600

    init.el: typo

--- extra/configs/.config/emacs/init.el
+++ extra/configs/.config/emacs/init.el
@@ -21,7 +21,7 @@
 (add-hook 'sh-mode-hook
 	  (lambda ()
 	    (setq sh-basic-offset 4)
-	    (setq sh-indentaion 4)
+	    (setq sh-indentation 4)
 	    (setq indent-tabs-mode nil)))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
